e64df4f3c215e8c0ba1d2899a4a60bb81094e42c
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2023-03-13 03:12:44 +0200

    first commit

---
+++ timer.c
@@ -0,0 +1,83 @@
+/*
+ * Timer.c
+ *
+ *  Author: Mohamed Sameh
+ */ 
+
+#include "timer.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+void timer0_normalmode_init()
+{
+	//preload value
+	TCNT0 = 0;
+	//PRESCALER -> 64
+	SET_BIT(TCCR0, CS00);
+	SET_BIT(TCCR0, CS01);
+	CLR_BIT(TCCR0, CS02);
+	//enable interrupt
+	sei();
+	SET_BIT(TIMSK,TOIE0);
+}
+
+
+void timer0_CTC_init()
+{
+	//select CTC mode
+	SET_BIT(TCCR0,WGM01);
+	//load a value in OCR0 
+	OCR0 = 78;
+	//PRESCALER -> 1024
+	SET_BIT(TCCR0, CS00);
+	SET_BIT(TCCR0, CS02);
+	//
+	//enable interrupt
+	sei();
+	SET_BIT(TIMSK,OCIE0);
+}
+
+void timer0_deinit()
+{
+	//PRESCALER -> NOCLOCK SOURCE
+	CLR_BIT(TCCR0, CS00);
+	CLR_BIT(TCCR0, CS01);
+	CLR_BIT(TCCR0, CS02);
+	//disable interrupt
+	CLR_BIT(TIMSK, TOIE0);
+}
+
+void timer1_fastPWM_A(double v)
+{
+	//set OC1A as output pin 
+	SET_BIT(DDRD,5);
+	//select fastPWM mode
+	SET_BIT(TCCR1A,WGM11);
+	SET_BIT(TCCR1B,WGM12);
+	SET_BIT(TCCR1B,WGM13);
+	//
+	OCR1A = v*1000;
+	ICR1 = 20000; 
+	//select timer clock, no prescaling
+	SET_BIT(TCCR1B,CS10);
+	//clear OC1A on compare match, set OC1A at BOTTOM
+	SET_BIT(TCCR1A,COM1A1);
+}
+
+void timer1_fastPWM_B(double v)
+{
+	//set OC1B as output pin
+	SET_BIT(DDRD,4);
+	//select fastPWM mode
+	SET_BIT(TCCR1A,WGM11);
+	SET_BIT(TCCR1B,WGM12);
+	SET_BIT(TCCR1B,WGM13);
+	//
+	OCR1A = v*1000;
+	ICR1 = 20000;
+	//select timer clock, no prescaling
+	SET_BIT(TCCR1B,CS10);
+	//clear OC1B on compare match, set OC1b at BOTTOM
+	SET_BIT(TCCR1A,COM1B1);
+}
---
+++ timer.h
@@ -0,0 +1,21 @@
+/*
+ * timer.h
+ *
+ *  Author: Mohamed Sameh
+ */ 
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+#include "Bit_Math.h"
+
+void timer0_normalmode_init();
+void timer0_CTC_init();
+void timer0_deinit();
+void timer1_fastPWM_A(double v);
+void timer1_fastPWM_B(double v);
+
+
+
+
+#endif /* TIMER_H_ */

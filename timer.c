636ca2d90137100565dec33336bf947b2ea7f729
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2023-12-09 17:26:33 +0200

    Add files via upload

--- timer.c
+++ timer.c
@@ -1,43 +1,48 @@
 /*
- * Timer.c
+ * timer.c
  *
  *  Author: Mohamed Sameh
  */ 
 
 #include "timer.h"
-#include <avr/io.h>
-#include <avr/interrupt.h>
 
-
+/**
+ * @brief Initializes the Normal Mode in Timer0
+ * 
+ */
 void timer0_normalmode_init()
 {
-	//preload value
-	TCNT0 = 0;
-	//PRESCALER -> 64
+	
+	//PRESCALER -> 1024
 	SET_BIT(TCCR0, CS00);
-	SET_BIT(TCCR0, CS01);
-	CLR_BIT(TCCR0, CS02);
-	//enable interrupt
+	SET_BIT(TCCR0, CS02);
+	//Enable interrupt
 	sei();
 	SET_BIT(TIMSK,TOIE0);
 }
 
-
+/**
+ * @brief Initializes the Clear Timer on Compare Mode in Timer0
+ * 
+ */
 void timer0_CTC_init()
 {
-	//select CTC mode
+	//Select CTC mode
 	SET_BIT(TCCR0,WGM01);
-	//load a value in OCR0 
-	OCR0 = 78;
+	//Load a value in OCR0 
+	OCR0=78;
 	//PRESCALER -> 1024
 	SET_BIT(TCCR0, CS00);
 	SET_BIT(TCCR0, CS02);
-	//
-	//enable interrupt
+	//Enable interrupt
 	sei();
 	SET_BIT(TIMSK,OCIE0);
 }
 
+/**
+ * @brief Deinitializes the Timer0
+ * 
+ */
 void timer0_deinit()
 {
 	//PRESCALER -> NOCLOCK SOURCE
@@ -48,36 +53,37 @@ void timer0_deinit()
 	CLR_BIT(TIMSK, TOIE0);
 }
 
+
 void timer1_fastPWM_A(double v)
 {
-	//set OC1A as output pin 
-	SET_BIT(DDRD,5);
-	//select fastPWM mode
+	//Sets OC1A as output pin 
+	dio_set_pin_direction(PULSE_PORT_A,PULSE1_PIN_A,DIO_DIRECTION_OUTPUT);
+	//Selects fastPWM mode
 	SET_BIT(TCCR1A,WGM11);
 	SET_BIT(TCCR1B,WGM12);
 	SET_BIT(TCCR1B,WGM13);
 	//
 	OCR1A = v*1000;
 	ICR1 = 20000; 
-	//select timer clock, no prescaling
+	//Selects timer clock, no prescaling
 	SET_BIT(TCCR1B,CS10);
-	//clear OC1A on compare match, set OC1A at BOTTOM
+	//Clears OC1A on compare match, set OC1A at BOTTOM
 	SET_BIT(TCCR1A,COM1A1);
 }
 
 void timer1_fastPWM_B(double v)
 {
-	//set OC1B as output pin
-	SET_BIT(DDRD,4);
-	//select fastPWM mode
+	//Sets OC1B as output pin
+	dio_set_pin_direction(PULSE_PORT_B,PULSE1_PIN_B,DIO_DIRECTION_OUTPUT);
+	//selects fastPWM mode
 	SET_BIT(TCCR1A,WGM11);
 	SET_BIT(TCCR1B,WGM12);
 	SET_BIT(TCCR1B,WGM13);
 	//
-	OCR1A = v*1000;
+	OCR1B = v*1000;
 	ICR1 = 20000;
-	//select timer clock, no prescaling
+	//Selects timer clock, no prescaling
 	SET_BIT(TCCR1B,CS10);
-	//clear OC1B on compare match, set OC1b at BOTTOM
+	//Clears OC1B on compare match, set OC1B at BOTTOM
 	SET_BIT(TCCR1A,COM1B1);
 }
--- timer.h
+++ timer.h
@@ -7,15 +7,54 @@
 
 #ifndef TIMER_H_
 #define TIMER_H_
-#include "Bit_Math.h"
 
+/* Section : Includes */
+#include <avr/interrupt.h>
+#include "dio.h"
+
+/* Section : Macro Declarations */
+#define PULSE_PORT_A		DIO_PORTD
+#define PULSE1_PIN_A		DIO_PIN5
+
+#define PULSE_PORT_B		DIO_PORTD
+#define PULSE1_PIN_B		DIO_PIN4
+
+/* Section : Macro Functions Declarations */
+
+/* Section : Data Types Declarations  */
+
+/* Section : Functions Declarations */
+/**
+ * @brief Initializes the Normal Mode in Timer0
+ * 
+ */
 void timer0_normalmode_init();
+
+/**
+ * @brief Initializes the Clear Timer on Compare Mode in Timer0
+ * 
+ */
 void timer0_CTC_init();
+
+/**
+ * @brief Deinitializes the Timer0
+ * 
+ */
 void timer0_deinit();
+
+/**
+ * @brief 
+ * 
+ * @param v 
+ */
 void timer1_fastPWM_A(double v);
+
+/**
+ * @brief 
+ * 
+ * @param v 
+ */
 void timer1_fastPWM_B(double v);
-
-
 
 
 #endif /* TIMER_H_ */
